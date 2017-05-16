7bf6624b96a9c4ff313cfee673f20cdcc2b0f671
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-05-16 20:37:19 +0200

    extract name and url for every startup

--- main.py
+++ main.py
@@ -4,20 +4,40 @@ from bs4 import BeautifulSoup
 cities = ["berlin", "hong-kong", "london", "melbourne", "sao-paulo", "stockholm", "sydney",
 "toronto", "vancouver"]
 
-for city in cities:
-    website     = "http://{}.startups-list.com".format(city)
-    soup        = soupify_website(site_url=website)
-    startups    = get_all_startups(soup)
+def get_all_startups(soup):
 
+    startups_list   = soup.find_all("div", class_="startup")
+    startups        = []
 
+    for startup in startups_list:
+        startup_url     = startup["data-name"]
+        startup_name    = startup["data-href"]
+        startups.append({"name": startup_name, "url": startup_url})
+
+    return startups
 
 def soupify_website(site_url=None):
 
     if site_url is not None:
         sauce   = requests.get(website).text
-        return BeauitulSoup(sauce, "html.parser")
+        return BeautifulSoup(sauce, "html.parser")
     else:
         raise ValueError("Argument site_url is required.")
 
 def get_jobs():
     pass
+
+for city in cities:
+    website     = "http://{}.startups-list.com".format(city)
+
+    print("")
+    print("Scraping {}".format(website))
+    print("")
+    soup        = soupify_website(site_url=website)
+
+    print("Getting startups info ...")
+    print("")
+    startups    = get_all_startups(soup)
+
+    print("Found {} startups in {}".format( len(startups), city))
+
